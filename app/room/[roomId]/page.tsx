96f47f1455b92bfa21fff5bd334c5b75ce4bb51e
Sayan650 <sayan650@gmail.com>
2024-01-14 02:05:40 +0530

    login and home page

---
+++ app/room/[roomId]/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { fetchUserById, getUserSession } from "../../services/user.service";
+import { useParams } from "next/navigation";
+import { fetchDrawingRoomById } from "../../services/drawing-room.service";
+import Navbar from "../../components/Navbar";
+
+const DrawingRoomPage = () => {
+  const { roomId } = useParams();
+  const [owner, setOwner] = useState<any | null>(null);
+  const [room, setRoom] = useState<any>([]);
+  const [user, setUser] = useState<any>({});
+  const [session, setSession] = useState<any>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [participantCount, setParticipantCount] = useState<number>(0);
+
+  useEffect(() => {
+    getUserSession().then((session) => {
+      setSession(session);
+      setUser(session?.user);
+
+      fetchDrawingRoomById(roomId as string).then((room) => {
+        const canEnterRoom =
+          room![0].isPublic || room![0].owner === session?.user?.id;
+
+        if (!canEnterRoom) {
+          return (window.location.href = "/");
+        }
+        setRoom(room![0]);
+        setIsLoading(false);
+        fetchUserById(room![0].owner).then((res) => {
+          setOwner(res.user);
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <main>
+      <Navbar
+        session={session}
+        owner={owner}
+        room={room}
+        isRoom
+        isLoadingRoom={isLoading}
+        participantCount={participantCount}
+      />
+    </main>
+  );
+};
+
+export default DrawingRoomPage;
